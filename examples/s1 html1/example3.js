8050fbbb16fc4a535d8baa054b81550fc48ce5fc
adish muhammed <adishmuhammedmm@gmail.com>
2023-08-24 18:19:35 +0530

    changed

---
+++ examples/s1 html1/example3.js
@@ -0,0 +1,11 @@
+// This is a single-line comment.
+
+/* This is a
+   multi-line comment */
+
+   var myName = "Bard";
+
+   // This is a comment that explains what the variable is for.
+   
+   console.log("My name is " + myName);
+   
